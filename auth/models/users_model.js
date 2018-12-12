1001b7362691fcd64e4ac59370bab3b18a4a6beb
connor981332 <connorbunch@outlook.com>
2018-12-12 06:16:51 +0000

    updating database schema

--- auth/models/users_model.js
+++ auth/models/users_model.js
@@ -2,8 +2,10 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 var UserSchema = new Schema({
     username: { type: String, unique: true },
+    name: String,
     email: String,
     color: String,
-    hashed_password: String
+    hashed_password: String,
+    high_score: {type: Number, default:0}
 });
 mongoose.model('User', UserSchema);
